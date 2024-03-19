5d8da46a513b6f584d7049fef166fa303b3485de
Danicode <ariyoesther2009@gmail.com>
2024-03-19 08:50:18 +0100

    second commit

--- function.js
+++ function.js
@@ -5,7 +5,7 @@ function createTable() {
 
     tab.innerHTML = ''
     
-    for (let index = input1.value; index <= input2.value; index++) {
+    for (let index = Number(input1.value); index <= Number(input2.value); index++) {
         for(let i = 0; i <= 12; i++){
       tab.innerHTML += `<p> ${index} x ${i} = ${index * i} </p>`
    }
